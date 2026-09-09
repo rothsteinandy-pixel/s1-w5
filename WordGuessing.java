5d57bc350bc7ab985cbf39f574678a8f46704fbe
Andy <rothstein.andy@gmail.com>
2026-09-09 11:40:34 -0700

    uhhbuuh

--- WordGuessing.java
+++ WordGuessing.java
@@ -1,18 +1,21 @@
 public class WordGuessing {
-    private String wordToBeGuessed;
-    private String userString = "_____";// 5 underscores
-  
-    //we will say that secret must be five characters
-    public WordGuessing(String secret) {
-  
-    }
-  
-    public void update(String letter) {
-  
-    }
-  
-    public String getUserString(){
-      
-    }
+  private String wordToBeGuessed;
+  private String userString = "_____";// 5 underscores
+
+  // we will say that secret must be five characters
+  public WordGuessing(String secret) {
+    wordToBeGuessed = secret;
   }
-  
+
+  // letter is in word, no duplicates
+  // substring (1 param): substring (2 param),indexOf, length
+  public void update(String letter) {
+    int letPosition = wordToBeGuessed.indexOf(letter);
+    userString = userString.substring(0,letPosition) + letter + userString.substring(letPosition + 1);
+  }
+
+  public String getUserString() {
+    return userString;
+  }
+}
+ 
--- WordGuessingMain.java
+++ WordGuessingMain.java
@@ -1,5 +1,12 @@
 public class WordGuessingMain{
     public static void main(String[] args){
-        
+      WordGuessing w = new WordGuessing( "witch");
+      w.update("i");
+      w.update("w");
+      w.update("c");
+      w.update("h");
+      w.update("t");
+      System.out.println(w.getUserString());
     }
+    
 }
